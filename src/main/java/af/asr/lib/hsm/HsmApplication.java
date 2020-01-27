752e0ced2ebdfe3374d32a4fa85c29917867a711
mohbadar <mohammadbadarhashimi@gmail.com>
2020-01-27 12:40:24 +0430

    hsm utility library of anar framework

---
+++ src/main/java/af/asr/lib/hsm/HsmApplication.java
@@ -0,0 +1,13 @@
+package af.asr.lib.hsm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HsmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HsmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/af/asr/lib/hsm/HsmApplicationTests.java
@@ -0,0 +1,13 @@
+package af.asr.lib.hsm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HsmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
