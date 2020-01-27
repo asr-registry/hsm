320f58fb526fc90fc738d2755ec7e8e0b4c7ee79
mohbadar <mohammadbadarhashimi@gmail.com>
2020-01-27 12:44:54 +0430

    thales

---
+++ src/main/java/af/asr/lib/hsm/thales/ThalesHSMService.java
@@ -0,0 +1,72 @@
+package af.asr.lib.hsm.thales;
+
+import org.util.hsm.api.CVVService;
+import org.util.hsm.api.HSMService;
+import org.util.hsm.api.IBMService;
+import org.util.hsm.api.KeyService;
+import org.util.hsm.api.MACService;
+import org.util.hsm.api.PVVService;
+import org.util.hsm.api.ThalesService;
+import org.util.hsm.api.TranslationService;
+
+public final class ThalesHSMService implements HSMService {
+
+	private final CVVService         cvvService         = new ThalesCVVService();
+	private final IBMService         ibmService         = new ThalesIBMService(this);
+	private final ThalesService      thalesService      = new ThalesServiceImpl();
+	private final KeyService         keyService         = new ThalesKeyService();
+	private final PVVService         pvvService         = new ThalesPVVService(this);
+	private final TranslationService translationService = new ThalesTranslationService();
+	private final MACService         macService         = new ThalesMACService();
+
+	@Override
+	public final String getName() {
+		return "THALES";
+	}
+
+	@Override
+	public final TranslationService translator() {
+		return translationService;
+	}
+
+	@Override
+	public final CVVService cvv() {
+		return cvvService;
+	}
+
+	@Override
+	public final KeyService key() {
+		return keyService;
+	}
+
+	@Override
+	public final ThalesService thales() {
+		return thalesService;
+	}
+
+	@Override
+	public final PVVService pvv() {
+		return pvvService;
+	}
+
+	@Override
+	public final IBMService ibm() {
+		return ibmService;
+	}
+
+	@Override
+	public final boolean shutdown() {
+		return true;
+	}
+
+	@Override
+	public final MACService mac() {
+		return macService;
+	}
+
+	@Override
+	public final String getResponseDescription(String code) {
+		return ThalesResponseDescription.codemap.get(code);
+	}
+
+}
---
+++ src/main/java/af/asr/lib/hsm/thales/ThalesServiceImpl.java
@@ -0,0 +1,51 @@
+package af.asr.lib.hsm.thales;
+
+import org.util.hsm.api.HSMConfig;
+import org.util.hsm.api.ThalesHSMConnect;
+import org.util.hsm.api.ThalesService;
+import org.util.hsm.api.constants.PinBlockFormat;
+import org.util.hsm.api.model.HSMResponse;
+import org.util.hsm.api.util.Strings;
+import org.util.nanolog.Logger;
+
+//@formatter:off
+public final class ThalesServiceImpl implements ThalesService {
+
+	@Override
+	public final HSMResponse encryptPinUnderLMK(final HSMConfig hsmConfig, String pan12, String pin, Logger logger) {
+		try {
+			String            pinPadded   = Strings.padRight(new StringBuilder(pin), 'F', hsmConfig.lengthOfPinLMK);
+			String            command     = new StringBuilder(23).append("0000BA").append(pinPadded).append(pan12).toString();
+			final String      response    = ThalesHSMConnect.send(hsmConfig, command, logger);
+			final HSMResponse hsmResponse = new HSMResponse(response.substring(6, 8));
+			if (hsmResponse.isSuccess) hsmResponse.value = response.substring(8);
+			return hsmResponse;
+		} catch (Exception e) {logger.error(e);}
+		return HSMResponse.IO;
+	}
+
+	@Override
+	public final HSMResponse encryptPinblockUnderLMK(HSMConfig hsmConfig, String pan12, String pinblock, String tpk, PinBlockFormat format, Logger logger) {
+		try {
+			String            command     = new StringBuilder(23).append("0000JC").append(tpk).append(pinblock).append(format).append(pan12).toString();
+			final String      response    = ThalesHSMConnect.send(hsmConfig, command, logger);
+			final HSMResponse hsmResponse = new HSMResponse(response.substring(6, 8));
+			if (hsmResponse.isSuccess) hsmResponse.value = response.substring(8, 8 + hsmConfig.lengthOfPinLMK);
+			return hsmResponse;
+		} catch (Exception e) {logger.error(e);}
+		return HSMResponse.IO;
+	}
+
+	@Override
+	public final HSMResponse decryptPinUnderLMK(HSMConfig hsmConfig, String pan12, String pinlmk, Logger logger) {
+		try {
+			String            command     = new StringBuilder(23).append("0000NG").append(pan12).append(pinlmk).toString();
+			final String      response    = ThalesHSMConnect.send(hsmConfig, command, logger);
+			final HSMResponse hsmResponse = new HSMResponse(response.substring(6, 8));
+			if (hsmResponse.isSuccess) hsmResponse.value = response.substring(8, 8 + hsmConfig.lengthOfPinLMK).replaceAll("F", "");
+			return hsmResponse;
+		} catch (Exception e) {logger.error(e);}
+		return HSMResponse.IO;
+	}
+
+}
